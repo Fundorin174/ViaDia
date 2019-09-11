d951bc67b393020bc399b875909e732d576d5d55
Fundorin174 <Fundorin174@gmail.com>
2019-09-11 19:10:45 +0300

    1109

--- js/script.js
+++ js/script.js
@@ -52,5 +52,66 @@ function squreBlocks () {
 
 squreBlocks();
 
+// Изменеие высоты картинок при изменении ширины окна браузерв
+window.addEventListener('resize', squreBlocks);
+
+
+
+
+
+//Слайдер отзывов
+//slider
+
+let slideIndex = 1,
+sliders = document.querySelectorAll('.slider-item'),
+prev = document.querySelector('.prev'),
+next = document.querySelector('.next'),
+dots = document.querySelectorAll('.dot'),
+dotWrap = document.querySelector('.slider-dots');
+
+
+
+
+
+function showSlide(n) { //показ слайда, прячет остальные, меняет цвет активной точки, закольцовывает показ по кругу
+
+if (n > sliders.length) {
+  slideIndex = 1;
+}
+if (n < 1) {
+  slideIndex = sliders.length;
+}
+sliders.forEach((item) => item.style.display = 'none');
+
+dots.forEach((item) => item.classList.remove('dot-active'));
+sliders[slideIndex - 1].style.display = 'block';
+
+dots[slideIndex - 1].classList.add('dot-active');
+}
+
+function changeSlide(n) {
+showSlide(slideIndex += n); // Переход к следующему
+}
+
+function currentSlide(n) {
+showSlide(slideIndex = n); //Показ нужного слайда
+}
+
+prev.addEventListener('click', () => changeSlide(-1));
+next.addEventListener('click', () => changeSlide(1));
+
+showSlide(slideIndex);
+
+dotWrap.addEventListener('click', (e) => {
+for (let i = 0; i < sliders.length + 1; i++) {
+  if (e.target.classList.contains('dot') && e.target == dots[i - 1]) {
+    currentSlide(i); //Переход при нажатии на точки
+  }
+}
+});
+
+
+
+
 
 });
